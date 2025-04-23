b620f2ca24e9fbfd2a97fcb248eae3dd348467ca
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-23 01:32:30 +0000

    revison for loop

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -15,7 +15,12 @@ class DiceController < ApplicationController
   end
 
   def five_four
-    
+    @dice_rolls = []
+    5.times do 
+      @new_roll = rand(1..4)
+      @dice_rolls.push(@new_roll)
+    end
+
     render({ :template => "dice_templates/5d4"})
   end
 
