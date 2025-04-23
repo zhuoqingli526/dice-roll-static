557acab23e836be89e4baa17a7a775da6c16cf0c
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-23 01:25:54 +0000

    final_version

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,6 +1,26 @@
 class DiceController < ApplicationController
   def two_six
-    
+
     render({ :template => "dice_templates/2d6"})
+  end
+  
+  def two_ten
+    
+    render({ :template => "dice_templates/2d10"})
+  end
+
+  def one_twenty
+    
+    render({ :template => "dice_templates/1d20"})
+  end
+
+  def five_four
+    
+    render({ :template => "dice_templates/5d4"})
+  end
+
+  def homepage
+    
+    render({ :template => "dice_templates/homepage"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  
-  get ("/dice/2/6", { :controller => "dice", :action => "two_six"}) 
+
+  get("/dice/2/6", { :controller => "dice", :action => "two_six"}) 
+
+  get("/dice/2/10", { :controller => "dice", :action => "two_ten"}) 
+
+  get("/dice/1/20", { :controller => "dice", :action => "one_twenty"}) 
+
+  get("/dice/5/4", { :controller => "dice", :action => "five_four"}) 
+
+  get("/", { :controller => "dice", :action => "homepage"})
+
 
 end
